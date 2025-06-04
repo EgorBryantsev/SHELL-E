08a40f1a518a039586c442ce8102a953d5110091
Huy Hong <kevinhong.nl@gmail.com>
2025-06-04 23:23:53 +0200

    Started on classes for Node and Grid representation

---
+++ Path-planning/PathPlanning/src/Node.cs
@@ -0,0 +1,19 @@
+﻿public class Node
+{
+    public bool Walkable;
+    public int GridX, GridY;
+    public int GCost, HCost;
+    public float Cost = 1;
+    public Node Parent;
+    public Vector3 WorldPosition;
+
+    public int FCost => GCost + HCost;
+
+    public Node(bool walkable, int gridX, int gridY, Vector3 worldPosition)
+    {
+        Walkable = walkable;
+        WorldPosition = worldPosition;
+        GridX = gridX;
+        GridY = gridY;
+    }
+}
